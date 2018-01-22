8f400e24eca91d2e54c9d8b17fcc91edc51870d1
Aleksey Nemychenkov <alexejnemychenkov@gmail.com>
2018-01-22 22:44:35 +0300

    Final refinements

--- Quotes/AppDelegate.swift
+++ Quotes/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let url = "wss://quotes.exness.com:18400"
         let client = ExnessClient(urlString: url)
